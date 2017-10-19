ab30ba6780d1f6749658cfc5125b2d67d0eca647
Maksym Solomkin <solomkinmv@gmail.com>
2017-10-19 17:28:15 +0300

    Configure Zuul

--- Zuul/src/main/java/com/luxoft/training/spring/cloud/Zuul.java
+++ Zuul/src/main/java/com/luxoft/training/spring/cloud/Zuul.java
@@ -1,4 +1,15 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
 public class Zuul {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul.class, args);
+    }
 }
