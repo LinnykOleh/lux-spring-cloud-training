0fc12ef32454b7e8a42d900d82c51a229c61d3da
Maksym Solomkin <solomkinmv@gmail.com>
2017-10-19 13:54:50 +0300

    Implement CardService

--- CardService/src/main/java/com/luxoft/training/spring/cloud/CardRest.java
+++ CardService/src/main/java/com/luxoft/training/spring/cloud/CardRest.java
@@ -1,4 +1,20 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CardRest {
+    private final CardNumberGenerator cardNumberGenerator;
+
+    @Autowired
+    public CardRest(CardNumberGenerator cardNumberGenerator) {
+        this.cardNumberGenerator = cardNumberGenerator;
+    }
+
+    @RequestMapping("/create")
+    public String create() {
+        return cardNumberGenerator.generate();
+    }
 }
