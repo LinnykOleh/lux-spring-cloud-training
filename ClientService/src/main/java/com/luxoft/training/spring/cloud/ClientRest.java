d18520a7dde23ab8455de5726c370a03862f1dfa
Maksym Solomkin <solomkinmv@gmail.com>
2017-10-19 13:43:27 +0300

    Fix bugs

--- ClientService/src/main/java/com/luxoft/training/spring/cloud/ClientRest.java
+++ ClientService/src/main/java/com/luxoft/training/spring/cloud/ClientRest.java
@@ -1,7 +1,7 @@
 package com.luxoft.training.spring.cloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,21 +20,21 @@ public class ClientRest {
         this.clientRepository = clientRepository;
     }
 
-    @RequestMapping(name = "/create")
+    @RequestMapping("/create")
     public ClientEntity create(@RequestParam("name") String name) {
         return clientDAO.create(name);
     }
 
-    @RequestMapping(name = "/update/{id}")
-    public HttpStatus update(@RequestParam("name") String name, @PathVariable("id") Integer id) {
+    @RequestMapping("/update/{id}")
+    public ResponseEntity<Object> update(@RequestParam("name") String name, @PathVariable("id") Integer id) {
         boolean update = clientDAO.update(id, name);
         if (update) {
-            return HttpStatus.OK;
+            return ResponseEntity.ok().build();
         }
-        return HttpStatus.NOT_FOUND;
+        return ResponseEntity.notFound().build();
     }
 
-    @RequestMapping(name = "/delete/{id}")
+    @RequestMapping("/delete/{id}")
     public void delete(@PathVariable("id") Integer id) {
         clientRepository.delete(id);
     }
@@ -44,7 +44,7 @@ public class ClientRest {
         return clientRepository.findAll();
     }
 
-    @RequestMapping(name = "/get/{id}")
+    @RequestMapping("/get/{id}")
     public void get(@PathVariable("id") Integer id) {
         clientRepository.findOne(id);
     }
