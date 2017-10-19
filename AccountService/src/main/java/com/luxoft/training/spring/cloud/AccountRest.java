b09cd914414ed62662e6a9b4ef355d20f0bf5716
Maksym Solomkin <solomkinmv@gmail.com>
2017-10-19 13:50:23 +0300

    Implement Account Service

--- AccountService/src/main/java/com/luxoft/training/spring/cloud/AccountRest.java
+++ AccountService/src/main/java/com/luxoft/training/spring/cloud/AccountRest.java
@@ -1,4 +1,42 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
 public class AccountRest {
+    private final AccountDAO accountDAO;
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountRest(AccountDAO accountDAO, AccountRepository accountRepository) {
+        this.accountDAO = accountDAO;
+        this.accountRepository = accountRepository;
+    }
+
+    @RequestMapping("/create")
+    public void create(@RequestParam("id") Integer id) {
+        accountDAO.create(id);
+    }
+
+    @RequestMapping("/fund/{id}")
+    public void fund(@PathVariable("id") Integer id, @RequestParam("sum") BigDecimal sum) {
+        accountDAO.addBalance(id, sum.abs());
+    }
+
+    @RequestMapping("/checkout/{id}")
+    public void checkout(@PathVariable("id") Integer id, @RequestParam("sum") BigDecimal sum) {
+        accountDAO.addBalance(id, sum.abs().negate());
+    }
+
+    @RequestMapping("/get")
+    public List<AccountEntity> get(@PathVariable("clientId") Integer clientId) {
+        return accountRepository.findByClientId(clientId);
+    }
 }
