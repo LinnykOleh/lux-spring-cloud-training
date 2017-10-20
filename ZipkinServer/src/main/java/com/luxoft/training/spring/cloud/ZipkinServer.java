cc708966c438e73667984508450e1bb9ce6fb66e
Maksym Solomkin <solomkinmv@gmail.com>
2017-10-20 15:03:12 +0300

    Configure Zipkin

--- ZipkinServer/src/main/java/com/luxoft/training/spring/cloud/ZipkinServer.java
+++ ZipkinServer/src/main/java/com/luxoft/training/spring/cloud/ZipkinServer.java
@@ -1,4 +1,15 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinStreamServer
+@EnableZipkinServer
+@SpringBootApplication
 public class ZipkinServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServer.class, args);
+    }
 }
