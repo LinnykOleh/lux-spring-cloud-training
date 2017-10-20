299a036b5054617e90353d2680e5f6d5384bbfff
Maksym Solomkin <solomkinmv@gmail.com>
2017-10-20 12:16:55 +0300

    Configure Hystrix dashboard

--- Dashboard/src/main/java/com/luxoft/training/spring/cloud/Dashboard.java
+++ Dashboard/src/main/java/com/luxoft/training/spring/cloud/Dashboard.java
@@ -2,7 +2,9 @@ package com.luxoft.training.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrixDashboard
 @SpringBootApplication
 public class Dashboard {
     public static void main(String[] args) {
