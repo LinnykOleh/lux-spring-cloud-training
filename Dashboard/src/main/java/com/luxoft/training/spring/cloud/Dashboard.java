d66431e68622ac06a384f4f4e0d3a8ce53632637
Maksym Solomkin <solomkinmv@gmail.com>
2017-10-20 12:31:03 +0300

    Configure Turbine

--- Dashboard/src/main/java/com/luxoft/training/spring/cloud/Dashboard.java
+++ Dashboard/src/main/java/com/luxoft/training/spring/cloud/Dashboard.java
@@ -3,8 +3,10 @@ package com.luxoft.training.spring.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @EnableHystrixDashboard
+@EnableTurbine
 @SpringBootApplication
 public class Dashboard {
     public static void main(String[] args) {
