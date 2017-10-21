52c7c36f46554e539b91d7b4ec73980dd670677f
Maksym Solomkin <solomkinmv@gmail.com>
2017-10-21 17:08:05 +0300

    Add more processors

--- Aggregator/src/main/java/com/luxoft/training/spring/cloud/Aggregator.java
+++ Aggregator/src/main/java/com/luxoft/training/spring/cloud/Aggregator.java
@@ -3,5 +3,12 @@ package com.luxoft.training.spring.cloud;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.aggregate.AggregateApplicationBuilder;
 
+@SpringBootApplication
 public class Aggregator {
+    public static void main(String[] args) {
+        new AggregateApplicationBuilder()
+                .from(WordNumSplitter.class)
+                .via(Processor.class)
+                .run(args);
+    }
 }
